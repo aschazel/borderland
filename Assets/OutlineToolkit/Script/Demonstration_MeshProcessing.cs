d088e63166c93adbcfecbe252f88590bf5679f0f
kiyuuwaii <fauzancdw589@gmail.com>
2023-05-20 17:43:41 +0700

    Update monitor ama kotak box

--- Assets/OutlineToolkit/Script/Demonstration_MeshProcessing.cs
+++ Assets/OutlineToolkit/Script/Demonstration_MeshProcessing.cs
@@ -21,7 +21,7 @@ public class Demonstration_MeshProcessing : MonoBehaviour {
 
             f.AddComponent<MeshFilter>().mesh = r;
             f.AddComponent<MeshRenderer>().material = mat;
-            f.GetComponent<MeshRenderer>().material.SetFloat("_Width", 0.05f / o.transform.localScale.x);
+            f.GetComponent<MeshRenderer>().material.SetFloat("_Width", 0.01f / o.transform.localScale.x);
         }
 	}
 }
